178a2fa42fd8137dba5bed8a57fdddc6f6a30b61
Noam Rivlin <noam.rivlin15@gmail.com>
2023-02-13 15:36:38 +0200

    Part III - Advanced Routes: Return Combined Data

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const baseUrl = 'https://api.unsplash.com/photos';
+const baseUrl = 'https://api.unsplash.com';
+const apiKey = process.env.UNSPLASH_ACCESS_KEY;
 
 router.route('/').get(async (req, res) => {
   try {
     const callPhotos = await axios.get(
-      `${baseUrl}?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+      `${baseUrl}/photos/?client_id=${apiKey}`
     );
     const data = await callPhotos.data;
     const rawPhotosURL = data.map((photo) => photo.urls.raw);
@@ -15,6 +16,42 @@ router.route('/').get(async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: 'Server error. Please try again later.' });
+  }
+});
+// username: 'weriaroslav'
+router.route('/user/:username').get(async (req, res) => {
+  try {
+    const { username } = req.params;
+    const callUserPhotos = await axios.get(
+      `${baseUrl}/users/${username}/photos/?client_id=${apiKey}`
+    );
+    // console.log('/////////////', callUserPhotos);
+    const userPhotosData = callUserPhotos.data;
+    // console.log('/////////////', userPhotosData);
+    const newArr = [];
+    const userPhotos = userPhotosData.map((photo) => {
+      // if (photo.description === null) {
+      //   photo.description = '"No description provided.'
+      // }
+      //  newArr.push(photo.id, photo.urls.raw, description )
+      return {
+        id: photo.id,
+        username,
+        url: photo.urls.raw,
+        description:
+          photo.description === null
+            ? 'No description provided.'
+            : photo.description,
+      };
+    });
+
+    res.json({ username, userPhotos });
+  } catch (error) {
+    // could there be more than one error? leaving it as an array an not error.response.data[0]
+    console.log('!!!!!!!!!!!!!!!!!!', error.response.data);
+    res.status(500).json({
+      message: error.response.data,
+    });
   }
 });
 
